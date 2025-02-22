9b55a53a52f7975fcb3a833c82c618ebcb18b4cd
devon1910 <ekpodave2000@gmail.com>
2025-02-23 00:43:35 +0100

    All done, Swagger UI jwt not working yet

--- SwiftLine.API/Controllers/BaseController.cs
+++ SwiftLine.API/Controllers/BaseController.cs
@@ -4,8 +4,9 @@ using System.Security.Claims;
 
 namespace SwiftLine.API.Controllers
 {
-    [ApiController]
+
     [Authorize]
+    [ApiController]
     [Route("api/v1/[controller]/[action]")]
     public class BaseController : ControllerBase
     {
--- SwiftLine.API/Program.cs
+++ SwiftLine.API/Program.cs
@@ -61,28 +61,29 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
 
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Name = "Authorization",
         In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
+        Description = "Place to add JWT with Bearer token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
         Scheme = "Bearer"
     });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
             {
-                Reference = new OpenApiReference
                 {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Name = "Bearer"
+                    }, new List<string>()
                 }
-            },
-            Array.Empty<string>()
-        }
-    });
+            });
     // Use string values for enums
     // options.SchemaFilter<EnumSchemaFilter>();
 
