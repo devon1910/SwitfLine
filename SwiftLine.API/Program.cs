37fdfc36c58f806b24572e84af8a054341875084
devon1910 <ekpodave2000@gmail.com>
2025-04-03 14:26:46 +0100

    Program,cs Refactored

--- SwiftLine.API/Program.cs
+++ SwiftLine.API/Program.cs
@@ -181,37 +181,6 @@ await DbSeeder.SeedData(app);  // Call this method to seed the data
 
 app.Run();
 
-internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
-{
-    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
-    {
-        var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
-        if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
-        {
-            var requirements = new Dictionary<string, OpenApiSecurityScheme>
-            {
-                ["Bearer"] = new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.Http,
-                    Scheme = "bearer",
-                    In = ParameterLocation.Header,
-                    BearerFormat = "Json Web Token",
-                    Description = "Place Your JWT Token",
-                }
-            };
-            document.Components ??= new OpenApiComponents();
-            document.Components.SecuritySchemes = requirements;
-            foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations))
-            {
-                operation.Value.Security.Add(new OpenApiSecurityRequirement
-                {
-                    [new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } }] = Array.Empty<string>()
-                });
-            }
-        }
-    }
-}
-
 public class LazyFactory<T> : Lazy<T> where T : class
 {
     public LazyFactory(IServiceProvider provider)
