ea70c0e04b0837267342ed126cc682da046f09c9
devon1910 <ekpodave2000@gmail.com>
2025-05-20 15:14:28 +0100

    Migrationn files for line member removal

---
+++ Infrastructure/Migrations/20250520130248_Columns name update and more logging fields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Columnsnameupdateandmoreloggingfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PositionInQueue",
+                table: "Lines",
+                newName: "PositionInQueueWhenJoined");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateLastUpdated",
+                table: "PushNotifications",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateLastUpdated",
+                table: "PushNotifications");
+
+            migrationBuilder.RenameColumn(
+                name: "PositionInQueueWhenJoined",
+                table: "Lines",
+                newName: "PositionInQueue");
+        }
+    }
+}
