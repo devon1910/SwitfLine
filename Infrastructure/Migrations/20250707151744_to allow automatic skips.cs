9d2e3b35cd59bd84efae2ef24ec5497e56738ff8
devon1910 <ekpodave2000@gmail.com>
2025-07-07 16:27:46 +0100

    Added Migration files

---
+++ Infrastructure/Migrations/20250707151744_to allow automatic skips.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class toallowautomaticskips : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowAutomaticSkips",
+                table: "Events",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowAutomaticSkips",
+                table: "Events");
+        }
+    }
+}
